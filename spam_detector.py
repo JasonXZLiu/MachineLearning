374acb9bd1815981b4c51b07b2d34644b22a5114
Jason Liu <jason.XZ.liu@outlook.com>
2018-10-08 11:55:18 -0400

    added stop_words

--- spam_detector.py
+++ spam_detector.py
@@ -1,10 +1,18 @@
 # spam detector using the SMS Spam Collection Dataset by UCI Machine Learning
 # https://www.kaggle.com/uciml/sms-spam-collection-dataset
 
+# data analysis and wrangling
 import pandas as pd
 import sklearn
-
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction import text 
+
+# machine learning
+from sklearn.naive_bayes import MultinomialNB
+
+# metrics 
+from sklearn import metrics
 
 spam = pd.read_csv("./spam.csv", encoding = "ISO-8859-1")
 
@@ -13,16 +21,14 @@ y = spam.v1
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
 
-from sklearn.feature_extraction.text import CountVectorizer
 vectorizer = CountVectorizer()
+vectorizer.stop_words = text.ENGLISH_STOP_WORDS
 X_train = vectorizer.fit_transform(X_train)
 
-from sklearn.naive_bayes import MultinomialNB
 mnb = MultinomialNB()
 mnb.fit(X_train, y_train)
 
 def test():
-    from sklearn import metrics
     y_pred = mnb.predict(vectorizer.transform(X_test))
     print("Multinomial Naive Bayes on Spam SMS Dataset Accuracy (in %):", metrics.accuracy_score(y_test, y_pred)*100)
     
